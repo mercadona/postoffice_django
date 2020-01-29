b2eb1a659908abb0fbab3f41d24be3c5e9549ce7
Imanol Cea <lonamiaec@users.noreply.github.com>
2020-01-29 12:37:49 +0100

    Update setup.py witn valid information (#5)

--- setup.py
+++ setup.py
@@ -11,24 +11,29 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name = 'postoffice_django',
     version = '0.1',
+    description = 'Postoffice django client',
+    long_description = README,
+    author = 'MercadonaTech',
+    author_email = 'software.online@mercadona.es',
+    url = 'http://github.com/mercadona/postoffice_django',
     packages=find_packages(exclude=("tests",)),
     include_package_data = True,
-    license = 'APACHE License', 
-    description = 'A simple Django app to comunicate with post office',
-    long_description = README,
-    url = 'http://www.example.com/',
-    author = 'Your Name',
-    author_email = 'yourname@example.com',
+    install_requires=[
+        'django',
+        'requests'
+    ]
+    license='Apache Software License 2.0',
+    keywords='postoffice'
     classifiers = [
         'Environment :: Web Environment',
-        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
