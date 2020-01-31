9b9e8391e67c70f94cfa1c7df12b0af82f88750b
juanjo <jponzvan@mercadona.es>
2020-01-31 17:12:38 +0100

    Add module to can check postoffice health

---
+++ postoffice_django/checks.py
@@ -0,0 +1,16 @@
+import requests
+from requests.exceptions import ConnectionError, ConnectTimeout
+
+from . import settings
+
+OK_STATUS = {'status': 'ok'}
+
+def health():
+    url = f'{settings.get_url()}/api/health/'
+
+    try:
+        response = requests.get(url, timeout=settings.get_timeout())
+    except (ConnectTimeout, ConnectionError):
+        return False
+
+    return response.json() == OK_STATUS
