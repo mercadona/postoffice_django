7b832e7098ccf4566a01354a70b9b3480d8a6a56
Sergio Revilla <replymusic@gmail.com>
2020-02-05 15:07:27 +0100

    Add coverage to test command

--- setup.py
+++ setup.py
@@ -2,24 +2,23 @@ import os
 
 from setuptools import find_packages, setup
 
-
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name = 'postoffice_django',
-    version = '0.3.0',
+    name='postoffice_django',
+    version='0.4.0',
     packages=find_packages(exclude=("tests",)),
-    include_package_data = True,
-    license = 'APACHE License',
-    description = 'A simple Django app to comunicate with post office',
-    long_description = README,
-    url = 'http://www.example.com/',
-    author = 'Mercadona',
-    author_email = 'sofware.online@mercadona.es',
-    classifiers = [
+    include_package_data=True,
+    license='APACHE License',
+    description='A simple Django app to comunicate with post office',
+    long_description=README,
+    url='http://www.example.com/',
+    author='Mercadona',
+    author_email='sofware.online@mercadona.es',
+    classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
