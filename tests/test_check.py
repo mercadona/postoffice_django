947495f39625b8637f331e917a15a1cd9481d347
jjponz <jponzvan@mercadona.es>
2020-02-05 18:48:36 +0100

    Add module to can check postoffice health (#8)

---
+++ tests/test_check.py
@@ -0,0 +1,42 @@
+import json
+from unittest.mock import patch
+
+import requests
+import responses
+from django.conf import settings
+
+from postoffice_django import checks
+
+POSTOFFICE_URL = settings.POSTOFFICE_URL
+
+class TestPostOfficeHealth:
+    def test_returns_true_when_postoffice_is_ok(self):
+        responses.add(responses.GET, f'{POSTOFFICE_URL}/api/health/',
+                      body=json.dumps({'status': 'ok'}))
+
+        assert checks.health() is True
+
+
+    def test_returns_false_when_postoffice_is_ko(self):
+        responses.add(responses.GET, f'{POSTOFFICE_URL}/api/health/',
+                      body=json.dumps({'status': 'ko'}))
+
+        assert checks.health() is False
+
+    def test_returns_false_when_postoffice_returns_internal_server_error(self):
+        responses.add(responses.GET, f'{POSTOFFICE_URL}/api/health/',
+                      body=json.dumps({}), status=500)
+
+        assert checks.health() is False
+
+    @patch('requests.get')
+    def test_returns_false_when_postoffice_raises_timeout(self, get_mock):
+        get_mock.side_effect = requests.exceptions.ConnectTimeout()
+
+        assert checks.health() is False
+
+    @patch('requests.get')
+    def test_returns_false_when_postoffice_raises_connection_error(self, get_mock):
+        get_mock.side_effect = requests.exceptions.ConnectionError()
+
+        assert checks.health() is False
