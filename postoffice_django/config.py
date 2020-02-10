1cf16ffe0280b47495f1f9a0072262906d8193e6
juanjo <jponzvan@mercadona.es>
2020-02-10 10:45:25 +0100

    Add missed type hinting

--- postoffice_django/config.py
+++ postoffice_django/config.py
@@ -1,24 +1,25 @@
 import logging
 
 import requests
+from responses import Response
 
 from . import settings
 
 logger = logging.getLogger(__name__)
 
 
-def configure_publishers():
+def configure_publishers() -> None:
 
     for consumer in settings.get_consumers():
         _create_publishers(consumer)
 
 
-def configure_topics():
+def configure_topics() -> None:
     for topic in settings.get_topics():
         _create_topic(topic)
 
 
-def _create_publishers(consumer):
+def _create_publishers(consumer: dict) -> None:
     url = f'{settings.get_url()}/api/publishers/'
     payload = {
         'active': True,
@@ -32,7 +33,7 @@ def _create_publishers(consumer):
     _save_creation_result_log(response)
 
 
-def _create_topic(topic_name):
+def _create_topic(topic_name: str) -> None:
     url = f'{settings.get_url()}/api/topics/'
     payload = {'name': topic_name, 'origin_host': settings.get_origin_host()}
 
@@ -40,7 +41,7 @@ def _create_topic(topic_name):
     _save_creation_result_log(response)
 
 
-def _save_creation_result_log(response):
+def _save_creation_result_log(response: Response) -> None:
     if response.status_code == 201:
         logger.info(f'{response.url} succesfully creation')
     else:
