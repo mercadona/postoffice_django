83ad1bba902e2812cef9b4d120fbea95504ebfbb
Imanol Cea <lonamiaec@users.noreply.github.com>
2020-04-14 10:42:38 +0200

    Create command to configure both topics and publishers (#39)

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -20,3 +20,14 @@ class TestConfigureTopicsCommand:
         call_command('configure_postoffice_topics')
 
         config_mock.assert_called()
+
+
+@pytest.mark.django_db
+class TestConfigurePostofficeCommand:
+    @patch('postoffice_django.config.configure_topics')
+    @patch('postoffice_django.config.configure_publishers')
+    def test_config_command(self, config_mock_topics, config_mock_publishers):
+        call_command('configure_postoffice')
+
+        config_mock_topics.assert_called()
+        config_mock_publishers.assert_called()
