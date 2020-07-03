da5c3ae84620133fcb9749f409d7bb5c54fc0367
Daniel Ruiz <druizca@mercadona.es>
2020-07-03 17:44:53 +0200

    Make is_postoffice_django_imported detect app in import statements

--- postoffice_django/introspection.py
+++ postoffice_django/introspection.py
@@ -1,3 +1,13 @@
+import ast
+
 
 def is_postoffice_django_imported(file_content: str) -> bool:
+    module_tree = ast.parse(file_content)
+
+    for node in module_tree.body:
+        if isinstance(node, ast.Import):
+            imported_module_names = [alias.name for alias in node.names]
+            if any(map(lambda name: name.split('.')[0] == 'postoffice_django', imported_module_names)):
+                return True
+
     return False
--- tests/test_introspection.py
+++ tests/test_introspection.py
@@ -7,3 +7,28 @@ class TestIsPostOfficeDjangoImported:
         file_content = ''
 
         assert not is_postoffice_django_imported(file_content)
+
+    def test_returns_true_when_postoffice_django_appears_in_import_statement(self):
+        file_content = 'import postoffice_django'
+
+        assert is_postoffice_django_imported(file_content)
+
+    def test_not_imported_when_module_contains_other_import_statements(self):
+        file_content = 'import datetime'
+
+        assert not is_postoffice_django_imported(file_content)
+
+    def test_imported_when_postoffice_django_submodule_is_imported(self):
+        file_content = 'import postoffice_django.publishing'
+
+        assert is_postoffice_django_imported(file_content)
+
+    def test_not_imported_when_module_name_contains_postoffice_django(self):
+        file_content = (
+            'import postoffice_django_lasjdf\n'
+            'import postoffice_django_lasjdf.publishing\n'
+            'import foo.postoffice_django\n'
+            'import foo.postoffice_django.bar\n'
+        )
+
+        assert not is_postoffice_django_imported(file_content)
