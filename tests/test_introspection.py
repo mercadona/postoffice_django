0df3641d6527015029d19497b52b92c06e39d1be
Daniel Ruiz <druizca@mercadona.es>
2020-07-03 17:50:43 +0200

    Arrange test code

--- tests/test_introspection.py
+++ tests/test_introspection.py
@@ -8,27 +8,30 @@ class TestIsPostOfficeDjangoImported:
 
         assert not is_postoffice_django_imported(file_content)
 
-    def test_imported_when_postoffice_django_appears_in_import_statement(self):
-        file_content = 'import postoffice_django'
+    
+    class TestFilesWithImportStatements:
 
-        assert is_postoffice_django_imported(file_content)
+        def test_imported_when_postoffice_django_appears_in_import_statement(self):
+            file_content = 'import postoffice_django'
 
-    def test_not_imported_when_module_contains_other_import_statements(self):
-        file_content = 'import datetime'
+            assert is_postoffice_django_imported(file_content)
 
-        assert not is_postoffice_django_imported(file_content)
+        def test_imported_when_postoffice_django_submodule_is_imported(self):
+            file_content = 'import postoffice_django.publishing'
 
-    def test_imported_when_postoffice_django_submodule_is_imported(self):
-        file_content = 'import postoffice_django.publishing'
+            assert is_postoffice_django_imported(file_content)
 
-        assert is_postoffice_django_imported(file_content)
+        def test_not_imported_when_module_contains_other_import_statements(self):
+            file_content = 'import datetime'
 
-    def test_not_imported_when_module_name_contains_postoffice_django(self):
-        file_content = (
-            'import postoffice_django_lasjdf\n'
-            'import postoffice_django_lasjdf.publishing\n'
-            'import foo.postoffice_django\n'
-            'import foo.postoffice_django.bar\n'
-        )
+            assert not is_postoffice_django_imported(file_content)
 
-        assert not is_postoffice_django_imported(file_content)
+        def test_not_imported_when_module_name_contains_postoffice_django(self):
+            file_content = (
+                'import postoffice_django_lasjdf\n'
+                'import postoffice_django_lasjdf.publishing\n'
+                'import foo.postoffice_django\n'
+                'import foo.postoffice_django.bar\n'
+            )
+
+            assert not is_postoffice_django_imported(file_content)
