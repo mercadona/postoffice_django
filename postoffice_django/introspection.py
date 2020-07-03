bf5eed2b7e469bc81b18220dc52bb466edeb59cf
Daniel Ruiz <druizca@mercadona.es>
2020-07-03 18:13:20 +0200

    Refactor function code

--- postoffice_django/introspection.py
+++ postoffice_django/introspection.py
@@ -1,5 +1,7 @@
 import ast
 
+from .apps import PostofficeDjangoConfig
+
 
 def is_postoffice_django_imported(file_content: str) -> bool:
     module_tree = ast.parse(file_content)
@@ -7,7 +9,12 @@ def is_postoffice_django_imported(file_content: str) -> bool:
     for node in module_tree.body:
         if isinstance(node, ast.Import):
             imported_module_names = [alias.name for alias in node.names]
-            if any(map(lambda name: name.split('.')[0] == 'postoffice_django', imported_module_names)):
+            if any(map(_is_postoffice_django_at_root_level, imported_module_names)):
                 return True
 
     return False
+
+def _is_postoffice_django_at_root_level(imported_module_name: str) -> bool:
+    root_level_module = imported_module_name.split('.')[0]
+
+    return root_level_module == PostofficeDjangoConfig.name
