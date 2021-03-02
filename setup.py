adc4a5aab1d484d4a7ac1bef2d3fbc409da41c79
Perizote <sergio.perizote@gmail.com>
2021-03-02 12:19:33 +0100

    Set version to the correct one 0.10.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(current_directory, 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
 
 
-VERSION = '0.19.0'
+VERSION = '0.10.0'
 
 
 class VerifyVersionCommand(install):
