6a8dc46c0f31c63c7fc50d4ed4cd01fe48e25b9a
jsansalo <jsansalo@mercadona.es>
2023-01-12 15:56:58 +0100

    circleci RC3

--- tests/settings.py
+++ tests/settings.py
@@ -10,7 +10,7 @@ ROOT_URLCONF = 'postoffice_django.urls'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_postoffice_django',
+        'NAME': 'test_postoffice_django_dev',
         'USER': 'postoffice_django',
         'PASSWORD': 'postoffice_django',
         'HOST': os.environ.get('PG_HOST', 'localhost'),
