01671fddfae47971e3191b9367784c63135b708b
jsansalo <jsansalo@mercadona.es>
2023-01-12 17:07:08 +0100

    circleci RC9

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,30 +4,30 @@ import pytest
 from django.core.management import call_command
 
 
-# @pytest.mark.django_db
-# class TestConfigurePublishersCommand:
-#     @patch('postoffice_django.config.configure_publishers')
-#     def test_config_command(self, config_mock):
-#         call_command('configure_postoffice_publishers')
+@pytest.mark.django_db
+class TestConfigurePublishersCommand:
+    @patch('postoffice_django.config.configure_publishers')
+    def test_config_command(self, config_mock):
+        call_command('configure_postoffice_publishers')
 
-#         config_mock.assert_called()
-
-
-# @pytest.mark.django_db
-# class TestConfigureTopicsCommand:
-#     @patch('postoffice_django.config.configure_topics')
-#     def test_config_command(self, config_mock):
-#         call_command('configure_postoffice_topics')
-
-#         config_mock.assert_called()
+        config_mock.assert_called()
 
 
-# @pytest.mark.django_db
-# class TestConfigurePostofficeCommand:
-#     @patch('postoffice_django.config.configure_topics')
-#     @patch('postoffice_django.config.configure_publishers')
-#     def test_config_command(self, config_mock_topics, config_mock_publishers):
-#         call_command('configure_postoffice')
+@pytest.mark.django_db
+class TestConfigureTopicsCommand:
+    @patch('postoffice_django.config.configure_topics')
+    def test_config_command(self, config_mock):
+        call_command('configure_postoffice_topics')
 
-#         config_mock_topics.assert_called()
-#         config_mock_publishers.assert_called()
+        config_mock.assert_called()
+
+
+@pytest.mark.django_db
+class TestConfigurePostofficeCommand:
+    @patch('postoffice_django.config.configure_topics')
+    @patch('postoffice_django.config.configure_publishers')
+    def test_config_command(self, config_mock_topics, config_mock_publishers):
+        call_command('configure_postoffice')
+
+        config_mock_topics.assert_called()
+        config_mock_publishers.assert_called()
