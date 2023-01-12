e9bbdf1313d84a18739ff6dd73d7d3c375baf1f0
jsansalo <jsansalo@mercadona.es>
2023-01-12 17:57:09 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(current_directory, 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
 
 
-VERSION = '0.12.0'
+VERSION = '0.12.1'
 
 
 class VerifyVersionCommand(install):
